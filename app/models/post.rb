0eabdbfdd77e8a2bec841223409852d0182a07da
gmujica <gregorymujica1993@gmail.com>
2017-03-07 02:34:38 -0400

    modulo de autenticacion terminado

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
-
   scope :most_recent, -> { order(id: :desc) }
 
+  belongs_to :author
+  
   def should_generate_new_friendly_id?
     title_changed?
   end
